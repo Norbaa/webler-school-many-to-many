f27a16273361003b98a90ab3646faf6d3b908423
norba <norbaa79@gmail.com>
2024-03-07 13:00:05 +0100

    create Course, School, Teacher and Student (using the relationship one-to-many or many-to-many)

---
+++ src/main/java/hu/webler/weblerschoolmanytomany/entity/Teacher.java
@@ -0,0 +1,24 @@
+package hu.webler.weblerschoolmanytomany.entity;
+
+import hu.webler.weblerschoolmanytomany.entity.base.Person;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teacher extends Person {
+
+    private String name;
+
+    @OneToMany(mappedBy = "teacher")
+    private List<Course> courses;
+}
